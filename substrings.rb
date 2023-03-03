cc8635bb8a40cfb81a9638522cda344be81cec40
Dylan Rice <dylanpatrickrice@gmail.com>
2023-03-03 14:55:53 +0000

    Implement the substring algorithm

---
+++ substrings.rb
@@ -0,0 +1,20 @@
+# defines string to test and dictionary to test against
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+test_string = "Howdy partner, sit down! How's it going?"
+
+# loops through the desired string and returns a hash containing the number of times each substring appeared inside the test string
+def substring (string, array)
+    string = string.downcase.split(" ")
+    string.reduce(Hash.new(0)) do |output, word_in_string|
+        array.reduce(output) do |output, word_in_dictionary|
+            if word_in_string.include?(word_in_dictionary)
+                output[word_in_dictionary] += 1
+            end
+            output
+        end
+        output
+    end
+end
+
+p test_string
+p substring(test_string, dictionary)
